a3ce774e091ca26e860a3f33c6bb18560571fd0d
Hajira  Mohamed <31967311+Hajira21@users.noreply.github.com>
2020-05-27 15:06:47 -0400

    Update groceries.js

--- SEG3125-Lab2/scripts/groceries.js
+++ SEG3125-Lab2/scripts/groceries.js
@@ -1,4 +1,5 @@
-/ Array of products, each product is an object with different fieldset
+	
+// Array of products, each product is an object with different fieldset
 // A set of ingredients should be added to products		 
 
 var products = [
