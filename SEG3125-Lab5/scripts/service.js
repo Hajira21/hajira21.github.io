a5317fe1691aba32dec824fe3356a7c2ba229730
Hajira21 <hmoha096@uottawa.ca>
2020-06-24 13:47:26 -0400

    unvailable days

--- SEG3125-Lab5/scripts/service.js
+++ SEG3125-Lab5/scripts/service.js
@@ -46,12 +46,22 @@ function validateCard(txtCard) {
 // The following code shows how to set specific dates to exclude, as well as Sundays (Day 0)
 // Make sure in your version that you associate Days to remove with Experts (e.g. John doesn't work Mondays)
 var unavailableDates = ["06/29/2020","07/07/2020","07/10/2020"]
+
+
 const setDateFormat = "mm/dd/yy";
 
 function disableDates(date) {
+    var stylist= document.getElementById("sel2").value;
+
     // Sunday is Day 0, disable all Sundays
-    if (date.getDay() == 0)
+    if (date.getDay() == 0 ||date.getDay()== 6 )
         return [false];
+    if( date.getDay()==1 && stylist=="Jasmine"){ // Jasmine doesnt work mondays
+        return [false];
+    }
+    if( date.getDay()==3 && stylist=="Brad"){ // Brad doesnt work mondays
+        return [false];
+    }
     var string = jQuery.datepicker.formatDate(setDateFormat, date);
     return [ unavailableDates.indexOf(string) == -1 ]
 }
@@ -90,6 +100,8 @@ $(document).ready(function(){
         }
     });
 
+   
+
     // To change the style of the calender, look in jqueryui.com, under Themes, in the ThemeRoller Gallery 
     // You can try different themes (the names are under the calendars) / This is Excite Bike 
     // To use a different theme you must include its css in your HTML file. 
@@ -97,7 +109,20 @@ $(document).ready(function(){
 
     // Also, here is a good tutorial for playing with the datepicker in https://webkul.com/blog/jquery-datepicker/ 
     // Datepicker is also documented as one of the widgets here: https://api.jqueryui.com/category/widgets/ 
-    $( "#datetimepicker1" ).datepicker(
+    $( "#datetimepicker1").datepicker(
+
+        {
+            dateFormat: setDateFormat,
+            // no calendar before June 1rst 2020
+            minDate: new Date('06/01/2020'),  
+            maxDate: '+4M',
+            // used to disable some dates
+            beforeShowDay: $.datepicker.noWeekends,
+            beforeShowDay: disableDates
+        }   
+    );
+    $( "#datetimepicker1").datepicker(
+
         {
             dateFormat: setDateFormat,
             // no calendar before June 1rst 2020
